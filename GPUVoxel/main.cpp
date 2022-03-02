cf7ee5f8d1c4548047572bb8db67dff508383b1d
YOomDev <ype.oomes@gmail.com>
2022-03-02 06:53:11 +0100

    Added math libraries, chunk structs and material struct

--- GPUVoxel/main.cpp
+++ GPUVoxel/main.cpp
@@ -1,15 +1,41 @@
-
+// std libraries
 #include <chrono> // timing
 #include <cstdio> // printf_s
+#include <vector>
 
+// OpenGL
 #define GLEW_STATIC
 #include <GL/glew.h>
 #include <GLFW/glfw3.h> // window, openglcontext
 
+// Math
+#include <glm/vec3.hpp>
+
 #define WIDTH 640
 #define HEIGHT 480
 
-static const char* computeshaderSource = "";
+struct Camera {
+    glm::vec3 position = { 0, 0, 0 };
+    glm::vec3 direction = { 0, 0, 0 };
+};
+
+struct ChunkData {
+    uint32_t voxel[16 * 16 * 16] = { 0 }; // to big to add location to this struct as that would overflow the max byte size of a object, so ChunkRef exists to add that functionality back
+};
+
+struct ChunkRef {
+    bool isUsed = false;
+    int location[3] = { 0,0,0 };
+};
+
+struct Material {
+    uint32_t type = 0; // 0 = AIR, 1 = SOLID, 2 = REFLECTIVE, 3 = REFRACTIVE
+    glm::vec3 color = {}; // unused in AIR
+    float roughness = 0.0f; // unused in AIR
+    float typeStrength = 0.0f; // only used on type = REFRACTIVE and type = AIR
+};
+
+static const char* computeshaderSource = ""; // TODO
 
 int main(void) {
     GLFWwindow* window;
@@ -37,6 +63,21 @@ int main(void) {
         return -1;
     }
 
+    // Setup data
+    Camera cam = { {-8,4,-8}, {4,-3,8} };
+    cam.direction /= cam.direction.length(); // normalize direction
+    Material material[4];
+    material[0] = { 0, {0.5,0.7,0.3}, 0.0f, 1.0f }; // AIR
+    material[0] = { 1, {0.3,0.5,0.7}, 0.3f, 1.0f }; // SOLID
+    material[0] = { 2, {0.3,0.7,0.5}, 0.1f, 1.0f }; // REFLECTIVE
+    material[0] = { 3, {0.7,0.3,0.5}, 0.0f, 1.325f }; // REFRACTIVE
+
+    // splitting the chunk data and location makes it not overflow
+    std::vector<ChunkRef> world;
+    std::vector<ChunkData> worldInfo;
+    world.push_back(ChunkRef());
+    worldInfo.push_back(ChunkData());
+
     // Setup compute shaders
     GLuint computeShader = glCreateShader(GL_COMPUTE_SHADER);
     glShaderSource(computeShader, 1, &computeshaderSource, NULL);
@@ -49,6 +90,12 @@ int main(void) {
 
     // Loop until the user closes the window
     while (!glfwWindowShouldClose(window)) {
+        // gameplay loop
+        // TODO
+
+        // Bind data to compute shader
+        // TODO
+
         // Clear screen
         glClear(GL_COLOR_BUFFER_BIT);
 
@@ -57,9 +104,9 @@ int main(void) {
         // TODO: use compute
         // one buffer for output,
         // another for chunk load requests
-        //glUseProgram(computeProgram);
-        //glDispathCompute(WIDTH, HEIGHT, 1);
-        //glMemoryBarrier(GL_ALL_BARRIER_BITS);
+        glUseProgram(computeProgram);
+        glDispatchCompute(WIDTH, HEIGHT, 1);
+        glMemoryBarrier(GL_ALL_BARRIER_BITS);
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
